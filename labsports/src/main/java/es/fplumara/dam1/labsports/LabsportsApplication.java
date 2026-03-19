431044f874fa72b92cf62ee53545cd6c308504cb
frc16 <153406170+frc16@users.noreply.github.com>
2026-03-19 19:07:11 +0100

    inicando proyecto

---
+++ labsports/src/main/java/es/fplumara/dam1/labsports/LabsportsApplication.java
@@ -0,0 +1,13 @@
+package es.fplumara.dam1.labsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsportsApplication.class, args);
+	}
+
+}
---
+++ labsports/src/test/java/es/fplumara/dam1/labsports/LabsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fplumara.dam1.labsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
